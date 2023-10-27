3f40f02f7a20bcafac6d7159605829ae374bb84c
Martin老師 <tpemartin@gmail.com>
2023-10-27 09:47:28 +0800

    Initial commit

---
+++ src/components/GalleryCard.jsx
@@ -0,0 +1,39 @@
+
+
+
+export default function GalleryCard(props) {
+
+
+    return (
+        <>
+        </>
+    )
+}
+
+function GalleryCardTitle(props) {
+    return (
+        <>
+        </>
+    )
+}
+
+function GalleryCardVideo(props) {
+    return (
+        <>
+        </>
+    )
+}
+
+function GalleryCardDescription(props) {
+    return (
+        <>
+        </>
+    )
+}
+
+function GalleryCardAction(props) {
+    return (
+        <>
+        </>
+    )
+}
